211a0b14c7e7a5e9594b4b654f528583040cb796
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-26 08:23:11 -0500

    Create Teorema_del_Valor_Intermedio_E3_LuisGerardoCardozoCarranza_Grupo3202.m

---
+++ Teorema_del_Valor_Intermedio_E3_LuisGerardoCardozoCarranza_Grupo3202.m
@@ -0,0 +1,29 @@
+% Octave Script
+% Title			:1.8 Teorema del Valor Intermedio
+% Description		:Script para graficar el ejericio 1 de la actividad 1.8
+% Author		:Luis Gerardo Cardozo Carranza (Gerard_CRS) luiscardozo161002@gmail.com
+% Date			:20210525
+% sion		        :1
+% Usage			:octave> /path/Teorema_del_Valor_Intermedio_Actividad_1.9_Luis Gerardo Cardozo Carranza_3202
+% Notes			:Requiere aplicacion octave usar en consola preferentemente.
+
+%limpia la pantalla y las variables.
+clc, clear  
+
+%Definimos la funcion
+x=-2:.05:4;
+f=inline('3*x-5');
+plot(x,f(x),'r','LineWidth',2)
+
+%Para calcular la integral
+valor_de_Integral=quad(f,-2,4)
+
+%Determina el valor del punto c
+Valor_Medio=strcat('3*x-5==',num2str(1/3*valor_de_Integral))
+
+%Interpretación geométrica
+hold on
+area(x,f(x))
+altura=f(c(1));
+plot([-2 4],[altura altura])
+hold off
